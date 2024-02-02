a89e7141d0470ba55ae3f54cf6879fc8a84c883f
KimHyeonBong <vongright@gmail.com>
2024-02-02 19:59:03 +0900

    PhiSquared quiz got me.

--- src/softeer/lv1/BallotCounting.java
+++ src/softeer/lv1/BallotCounting.java
@@ -1,8 +1,6 @@
 package softeer.lv1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BallotCounting {
@@ -10,7 +8,6 @@ public class BallotCounting {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = sc.nextInt();
         int[] candidates = new int[n];
@@ -20,7 +17,8 @@ public class BallotCounting {
 
         // assign.
         for (int i = 0; i < candidates.length; i++) {
-            candidates[i] = Integer.parseInt(br.readLine());
+
+            candidates[i] = sc.nextInt();
 
             remnants[i] = candidates[i] % 5;
             fives[i] = candidates[i] - remnants[i];
@@ -29,16 +27,20 @@ public class BallotCounting {
         }
 
         // String.repeat()
-        for (int i = 0; i < candidates.length; i ++) {
-            printArr[i] += "+++++ ".repeat(fives[i] / 5 >= 1 ? fives[i] / 5 : 0) +
+        for (int i = 0; i < candidates.length; i++) {
+            printArr[i] += "++++ ".repeat(fives[i] / 5 >= 1 ? (fives[i] / 5) : 0) +
                     "|".repeat(remnants[i]);
+            // remove last " " when candidates[...] % 5 == 0
+            if (remnants[i] == 0) {
+                printArr[i] = printArr[i].substring(0, (printArr[i].length() - 1));
+            }
             System.out.println(printArr[i]);
         }
 
         // builders with String.repeat()
-//        for (int i = 0; i < candidates.length; i ++) {
+//        for (int i = 0; i < candidates.length; i++) {
 //            StringBuilder builder = new StringBuilder(printArr[i]);
-//            builder.append("+++++ ".repeat(fives[i] / 5 >= 1 ? fives[i] / 5 : 0));
+//            builder.append("++++ ".repeat(fives[i] / 5 >= 1 ? fives[i] / 5 : 0));
 //            builder.append("|".repeat(remnants[i]));
 //            System.out.println(builder.toString());
 //        }
@@ -49,6 +51,7 @@ public class BallotCounting {
 //            for (int j = 0; j < fives[i]; j++) {
 //                if (j != 0 && j % 5 == 0) {
 //                    builder.append(" ");
+//                    continue;
 //                }
 //                builder.append("+");
 //            }
---
+++ src/softeer/lv3/PhiSquared.java
@@ -0,0 +1,135 @@
+package softeer.lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class PhiSquared {
+    public static void main(String[] args) throws IndexOutOfBoundsException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        List<Integer> keyList = new ArrayList<>();
+        List<Integer> valueList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count; i++) {
+            keyList.add(i + 1);
+            valueList.add(Integer.parseInt(st.nextToken()));
+        }
+
+
+        // System.out.println(keyList + " <- keyList");
+        // System.out.println(valueList + " <- valueList");
+
+        int pointer = 0;
+
+        while (true) {
+            // System.out.println(pointer + " <- pointer at.");
+
+            Integer currentValue;
+            Integer nextValue;
+            Integer previousValue;
+
+            if (pointer == 0) {
+                currentValue = valueList.get(pointer);
+                nextValue = valueList.get(pointer + 1);
+
+                if (currentValue >= nextValue) {
+                    valueList.set(pointer, currentValue + nextValue);
+
+                    keyList.remove(pointer + 1);
+                    valueList.remove(pointer + 1);
+
+                    // System.out.println("=====================");
+                    // System.out.println(keyList + " <- keyList");
+                    // System.out.println(valueList + " <- valueList");
+                    // System.out.println("=====================");
+                    // System.out.println(keyList.size() + " <- keyList.size() after set");
+
+                }
+
+            } else if (pointer == keyList.size() - 1) {
+                currentValue = valueList.get(valueList.size() - 1);
+                previousValue = valueList.get(valueList.size() - 2);
+
+                if (currentValue >= previousValue) {
+                    valueList.set(pointer, currentValue + previousValue);
+
+                    keyList.remove(pointer - 1);
+                    valueList.remove(pointer - 1);
+
+                    // System.out.println("=====================");
+                    // System.out.println(keyList + " <- keyList");
+                    // System.out.println(valueList + " <- valueList");
+                    // System.out.println("=====================");
+                    // System.out.println(keyList.size() + " <- keyList.size() after set");
+
+                }
+
+            } else {
+
+                currentValue = valueList.get(pointer);
+                nextValue = valueList.get(pointer + 1);
+                previousValue = valueList.get(pointer - 1);
+
+                if (currentValue >= nextValue && currentValue >= previousValue) {
+                    valueList.set(pointer, currentValue + previousValue + nextValue);
+
+                    keyList.remove(pointer + 1);
+                    keyList.remove(pointer - 1);
+                    valueList.remove(pointer + 1);
+                    valueList.remove(pointer - 1);
+
+                    // System.out.println("=====================");
+                    // System.out.println(keyList + " <- keyList");
+                    // System.out.println(valueList + " <- valueList");
+                    // System.out.println("=====================");
+                    // System.out.println(keyList.size() + " <- keyList.size() after set");
+
+                } else if (currentValue >= nextValue && currentValue < previousValue) {
+                    valueList.set(pointer, currentValue + nextValue);
+
+                    keyList.remove(pointer + 1);
+                    valueList.remove(pointer + 1);
+
+                    // System.out.println("=====================");
+                    // System.out.println(keyList + " <- keyList");
+                    // System.out.println(valueList + " <- valueList");
+                    // System.out.println("=====================");
+                    // System.out.println(keyList.size() + " <- keyList.size() after set");
+
+                } else if (currentValue < nextValue && currentValue >= previousValue) {
+                    valueList.set(pointer, currentValue + previousValue);
+
+                    keyList.remove(pointer - 1);
+                    valueList.remove(pointer - 1);
+
+                    // System.out.println("=====================");
+                    // System.out.println(keyList + " <- keyList");
+                    // System.out.println(valueList + " <- valueList");
+                    // System.out.println("=====================");
+                    // System.out.println(keyList.size() + " <- keyList.size() after set");
+
+                    pointer--;
+
+                }
+
+            }
+
+            if (pointer == keyList.size()) pointer = 0;
+            pointer++;
+            if (keyList.size() == 1) {
+                break;
+            }
+
+        } // while ends.
+
+        System.out.println(valueList.get(0));
+        System.out.println(keyList.get(0));
+
+    }
+
+}
